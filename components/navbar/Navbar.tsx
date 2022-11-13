eb620490b8aed69d1f2ea153f9541e92b2c6cee7
MarioDiCaprio <mariodicaprio10@gmail.com>
2022-11-13 23:50:53 +0100

    Redesigned Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {useRouter} from "next/router";
 import Link from "next/link";
+import {motion, useCycle, Variants} from "framer-motion"
+import {BsGithub as GitHubIcon} from "react-icons/bs";
 import styles from './Navbar.module.scss';
+
+
+const navbarVariants: Variants = {
+    open: (height = 1000) => ({
+        clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+        transition: {
+            type: "spring",
+            stiffness: 20,
+            restDelta: 2
+        }
+    }),
+    closed: {
+        clipPath: "circle(0px at 40px 40px)",
+        transition: {
+            type: "spring",
+            stiffness: 400,
+            damping: 40
+        }
+    }
+};
+
+
+interface NavbarMasterProps {
+    children?: ReactNode;
+}
+
+const NavbarMaster: React.FC<NavbarMasterProps> = ({ children }) => {
+    const [isOpen, toggleOpen] = useCycle<boolean>(false, true);
+
+    return (
+        <motion.nav animate={isOpen? 'open' : 'closed'}>
+
+            {/* Logo */}
+            <motion.div
+                className={styles.logoWrapper}
+                onClick={() => toggleOpen()}
+                whileTap={{ scale: 0.7 }}
+                whileHover={{ scale: 0.9 }}
+                transition={{ type: 'spring', duration: 0.3 }}
+            >
+                <img src="/logo.png"  alt=''/>
+            </motion.div>
+
+            <motion.div className={styles.navigation} variants={navbarVariants}>
+                { children }
+            </motion.div>
+
+        </motion.nav>
+    );
+}
 
 
 const Navbar: React.FC = () => {
@@ -12,13 +64,7 @@ const Navbar: React.FC = () => {
     }
 
     return (
-        <nav className={styles.context}>
-
-            {/* Logo */}
-            <div className={styles.logoWrapper}>
-                <img src="/logo.png"  alt=''/>
-            </div>
-
+        <NavbarMaster>
             {/* Pages */}
             <div className={styles.pagesWrapper}>
                 <Link href="/">
@@ -45,10 +91,12 @@ const Navbar: React.FC = () => {
 
             {/* Links */}
             <div className={styles.linksWrapper}>
-
+                <a href="https://github.com/MarioDiCaprioUni">
+                    <GitHubIcon />
+                </a>
             </div>
 
-        </nav>
+        </NavbarMaster>
     );
 }
 
